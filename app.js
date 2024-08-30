d497f1fe964842604b7afa066be52342f349c006
aygul2018 <teacher.aygunm@gmail.com>
2024-08-30 14:02:12 +0400

    hi!

---
+++ app.js
@@ -0,0 +1,12 @@
+let stockItem = {
+    id: "key",
+    place: "first floor",
+    weight: 100
+}
+
+console.log(stockItem);
+let b = {...stockItem, shopItem: 25 };
+console.log(stockItem);
+console.log(b);
+
+
